634848a41b01da76b940edc15dd9a53a36453f32
nnelli <nnelli@automatedinsights.com>
2017-02-02 14:43:13 -0500

    Added tests (I know, backwards work flow), though they dont seem to want to run

--- user_validator_test.rb
+++ user_validator_test.rb
@@ -8,4 +8,22 @@ class UserValidatorTest < Minitest::Test
     assert UserValidator.new('homework.csv')
   end
 
+  def test_joined
+    user = UserValidator.new('homework.csv')
+    bad_joined_dates = user.check_joined
+    assert bad_joined_dates == [1,4,6]
+  end
+
+  def test_email
+    user = UserValidator.new('homework.csv')
+    bad_emails = user.check_email
+    assert bad_emails == [1,2]
+  end
+
+  def test_phone
+    user = UserValidator.new('homework.csv')
+    bad_phone = user.check_phone
+    assert bad_emails == [0,4,6]
+  end
+
 end
